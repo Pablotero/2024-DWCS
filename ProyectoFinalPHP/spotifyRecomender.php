87fb56251626e8d5d677032f95c3265822235750
a23PAbloOF <a23PabloOF@iessanclemente.net>
2024-10-10 14:08:24 +0200

    10/10/2024 14:08

---
+++ ProyectoFinalPHP/spotifyRecomender.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="spotifyRecomender.css">
+    <title>SpotifyRecomender</title>
+</head>
+
+<body>
+    <div class="header">
+        <h1>Spotify Recomender</h1>
+    </div>
+
+    <section class="container">
+        
+    </section>
+</body>
+</html>
+
+
+<!--       <img src="Imagenes/Rauw/Rauw.jpeg" alt="">
+        <img src="Imagenes/MykeTowers/MykeTowers.jpeg" alt="">
+        <img src="Imagenes/NSQK/nsqk.jpeg" alt="">
+        <img src="Imagenes/Mora/Mora.jpeg" alt="">
+        <img src="Imagenes/Mix/FotoUsuario.png" alt="">
+        <img src="Imagenes/Jhayco/Jhayco.jpeg" alt="">
