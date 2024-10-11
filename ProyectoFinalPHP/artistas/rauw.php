d14a84c9f4c31fe5e3ead9341bb32dbf87d5d24f
a23PAbloOF <a23PabloOF@iessanclemente.net>
2024-10-11 13:41:15 +0200

    11/10/2024

---
+++ ProyectoFinalPHP/artistas/rauw.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SPOTIFY RECOMENDER</title>
+    <link rel="stylesheet" href="layoutArtistas.css">
+</head>
+<body>
+    <header>
+        <h1>Rauw Alejandro</h1>
+    </header>
+
+    <section class="container">
+        <div class="photo-row">
+            <div class="photo-box">
+                <a href="https://open.spotify.com/intl-es/track/110wwp2SRousmXjvx8zIoI?si=33cf80d173154a62"><p>PLAYA SATURNO: AL CALLAO</p></a>
+                <img src="../Imagenes/Rauw/Viceversa.jpeg" alt="">
+            </div>
+            <div class="photo-box">
+                <a href="https://open.spotify.com/intl-es/track/2CEf2gU5ZJamLXa3NrgAvA?si=41a92ff7a58049ae"><p>VICEVERSA: DESENFOCAO</p></a>
+                <img src="../Imagenes/Rauw/Saturno.jpeg" alt="Foto 2">
+            </div>
+            <div class="photo-box">
+                <a href="https://open.spotify.com/intl-es/track/1ImWmtI7DM2uFPNJHHbD8g?si=e575e05448ca4638"><p>SATURNO: DIME QUIÉN</p></a>
+                <img src="../Imagenes/Rauw/PlayaSaturno.jpeg" alt="Foto 3">
+            </div>
+        </div>
+    </section>
+</body>
+</html>
+
--- ProyectoFinalPHP/spotifyRecomender.php
+++ ProyectoFinalPHP/spotifyRecomender.php
@@ -1,22 +1,52 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="spotifyRecomender.css">
-    <title>SpotifyRecomender</title>
+    <title>Layout de Imágenes</title>
 </head>
-
 <body>
-    <div class="header">
-        <h1>Spotify Recomender</h1>
+    <header>
+        <h1>Mi Galería de Imágenes</h1>
+    </header>
+    <div class="container">
+        <div class="row">
+            <div class="image-card">
+                <img src="Imagenes/Rauw/Rauw.jpeg" alt="">
+                <p>Descripción 1</p>
+            </div>
+            <div class="image-card">
+                <img src="Imagenes/MykeTowers/MykeTowers.jpeg" alt="">
+                <p>Descripción 2</p>
+            </div>
+            <div class="image-card">
+                <img src="Imagenes/NSQK/nsqk.jpeg" alt="">
+                <p>Descripción 3</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="image-card">
+                <img src="Imagenes/Mora/Mora.jpeg" alt="">
+                <p>Descripción 4</p>
+            </div>
+            <div class="image-card">
+                <img src="Imagenes/Mix/FotoUsuario.png" alt="">
+                <p>Descripción 5</p>
+            </div>
+            <div class="image-card">
+                <img src="Imagenes/Jhayco/Jhayco.jpeg" alt="">
+                <p>Descripción 6</p>
+            </div>
+        </div>
     </div>
-
-    <section class="container">
-        
-    </section>
+    <div class="buttons">
+        <button>Botón 1</button>
+        <button>Botón 2</button>
+    </div>
 </body>
 </html>
+
 
 
 <!--       <img src="Imagenes/Rauw/Rauw.jpeg" alt="">
